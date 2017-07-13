861372fd3a51d6adf548124f3a2e1bdc8957ba23
zhao.liu <zhao.liu@zhaopin.com.cn>
2017-07-13 11:13:17 +0800

    添加LZPods

---
+++ CocosPods/LZlib/LZPods.h
@@ -0,0 +1,13 @@
+//
+//  LZPods.h
+//  CocosPods
+//
+//  Created by Liu Zhao on 2017/7/13.
+//  Copyright © 2017年 Liu Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LZPods : NSObject
+
+@end
---
+++ CocosPods/LZlib/LZPods.m
@@ -0,0 +1,13 @@
+//
+//  LZPods.m
+//  CocosPods
+//
+//  Created by Liu Zhao on 2017/7/13.
+//  Copyright © 2017年 Liu Zhao. All rights reserved.
+//
+
+#import "LZPods.h"
+
+@implementation LZPods
+
+@end
