0769af9005a3b63bb491d37dddb91724a1416f40
MMR无与伦比 <truend86@qq.com>
2018-03-25 17:29:41 +0800

    添加pod，依赖FMDB

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios,'8.0'
+target 'CocosPods' do
+    pod 'FMDB'
+end
---
+++ Pods/Target Support Files/Pods-CocosPods/Pods-CocosPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocosPods : NSObject
+@end
+@implementation PodsDummy_Pods_CocosPods
+@end
